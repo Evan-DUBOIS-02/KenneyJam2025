a9a95c47d535486e4e432227e02aa37527311d50
Evan DUBOIS <72149631+Evan-DUBOIS-02@users.noreply.github.com>
2025-07-17 09:13:32 +0200

    Add: Folders hierarchy, TMP lib, main/prod scenes, main menu scene with navigation to main level scene

---
+++ Assets/Scripts/UI/UI_Manager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI
+{
+    public class UI_Manager:MonoBehaviour
+    {
+        [SerializeField]
+        private string _playSceneName;
+        
+        public void OnClickPlay()
+        {
+            SceneManager.LoadScene(_playSceneName);
+        }
+    }
+}
