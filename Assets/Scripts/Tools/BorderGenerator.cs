a06e4b1d0c9081c411092f1ac13bde98307667d4
Evan DUBOIS <72149631+Evan-DUBOIS-02@users.noreply.github.com>
2025-07-19 00:23:32 +0200

    Base gameplay

---
+++ Assets/Scripts/Tools/BorderGenerator.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BorderGenerator : MonoBehaviour
+{
+    public List<GameObject> _bordersTop;
+    public List<GameObject> _bordersBot;
+    public List<Image> _images;
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        UpdateBorders();
+    }
+
+    void UpdateBorders()
+    {
+        for (int i = 0; i < _images.Count; i++)
+        {
+            GameObject border = _bordersTop[i];
+            border.transform.position = new Vector3(
+                Mathf.Lerp(-96, 96, _images[i].fillAmount),
+                border.transform.position.y,
+                border.transform.position.z);
+        }
+
+        for (int i = 0; i < _bordersBot.Count; i++)
+        {
+            GameObject border = _bordersBot[i];
+            border.transform.position = new Vector3(
+                (_bordersTop[i].transform.position.x + _bordersTop[i+1].transform.position.x)/2,
+                border.transform.position.y,
+                border.transform.position.z
+            );
+            border.transform.localScale = new Vector3(
+                border.transform.localScale.x,
+                border.transform.localScale.y,
+                _bordersTop[i].transform.position.x - _bordersTop[i+1].transform.position.x
+            );
+        }
+    }
+}
