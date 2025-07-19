e2305ffe81951ffedcba8f0e2ba84a4fb80132b9
Gugusea <augustin.guyot2@gmail.com>
2025-07-19 16:39:14 +0200

    Ajout boucle gameplay  condition de victoires+ UI / Credits

--- Assets/Scripts/UI/UI_Manager.cs
+++ Assets/Scripts/UI/UI_Manager.cs
@@ -7,10 +7,24 @@ namespace UI
     {
         [SerializeField]
         private string _playSceneName;
-        
+        [SerializeField]
+        private string _playSceneMainMenu;
+        [SerializeField]
+        private string _playSceneCredit;
+
         public void OnClickPlay()
         {
             SceneManager.LoadScene(_playSceneName);
+        }
+
+        public void BackToMainMenu()
+        {
+            SceneManager.LoadScene(_playSceneMainMenu);
+        }
+
+        public void CreditScene()
+        {
+            SceneManager.LoadScene(_playSceneCredit);
         }
     }
 }
